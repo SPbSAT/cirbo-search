74356c0e3b049a727d87bbe003bb60eb7c1d4505
spefk <f.kurmazov.b@gmail.com>
2025-10-17 02:23:47 +0300

    fix CI/CD: linter runs correctly

--- app/cirbo_search_cli.cpp
+++ app/cirbo_search_cli.cpp
@@ -1,22 +1,37 @@
 #include <CLI/CLI.hpp>
-#include <cstdint>
+#include <exception>
 #include <iostream>
 
 #include "core/logic.hpp"
 #include "core/operators.hpp"
 
-int main(int argc, char** argv)
+int main(int const argc, char** argv)
 {
-    CLI::App app{"Cirbo-Search CLI"};
+    try
+    {
+        CLI::App app{"Cirbo-Search CLI"};
 
-    bool verbose = false;
-    app.add_flag("-v,--verbose", verbose, "Verbose output");
-    CLI11_PARSE(app, argc, argv);
+        bool verbose = false;
+        app.add_flag("-v,--verbose", verbose, "Verbose output");
+        CLI11_PARSE(app, argc, argv);
 
-    std::cout << "Cirbo-Search CLI running" << (verbose ? " (verbose)" : "") << "\n";
+        std::cout << "Cirbo-Search CLI running" << (verbose ? " (verbose)" : "") << "\n";
 
-    // Just for includes testing.
-    std::cout << static_cast<int>(cirbo::GateType::AND) << std::endl;
+        // Just for includes testing.
+        std::cout << static_cast<int>(cirbo::GateType::AND) << std::endl;
+        std::cout << static_cast<int>(cirbo::op::AND(cirbo::GateState::FALSE, cirbo::GateState::TRUE)) << std::endl;
+    }
+    catch (std::exception const& exc)
+    {
+        std::cerr << "An exception occurred:\n";
+        std::cerr << exc.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception occurred." << std::endl;
+        return 1;
+    }
 
     return 0;
 }
