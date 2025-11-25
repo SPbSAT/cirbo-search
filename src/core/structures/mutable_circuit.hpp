697e08cf327c9935592b9bcf6108f0aa0f8f5b0f
Aleksandra Litvinenko <alexandralit09022005@gmail.com>
2025-11-25 10:54:16 +0300

    Add class MutableCircuit

--- src/core/structures/mutable_circuit.hpp
+++ src/core/structures/mutable_circuit.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <unordered_map>
 #include "core/structures/dag.hpp"
 #include "core/structures/gate_info.hpp"
 #include "core/structures/icircuit.hpp"
@@ -14,6 +15,14 @@ namespace cirbo
         GateType type = GateType::UNDEFINED;
         GateIdContainer operands;
         GateIdContainer users;
+
+        friend class MutableCircuit;
+    
+    protected:
+        void setId(GateId new_id)
+        {
+            id = new_id;
+        }
 
     public:
         MutableNode() = default;
@@ -175,5 +184,188 @@ namespace cirbo
             }
         }
     };
+
+    class MutableCircuit
+    {
+    private:
+        std::vector<MutableNode> gates;
+        GateIdContainer input_gates;
+        GateIdContainer output_gates;
+        GateId next_gate_id;
     
+    public:
+        MutableCircuit() = default;
+        ~MutableCircuit() = default;
+        MutableCircuit(MutableCircuit& m_circuit) = default;
+        MutableCircuit(MutableCircuit const& m_circuit) = default;
+        MutableCircuit& operator=(MutableCircuit const& m_circuit) = default;
+
+        MutableCircuit(MutableCircuit&& m_circuit)
+                : gates(std::exchange(m_circuit.gates, {}))
+                , input_gates(std::exchange(m_circuit.input_gates, {}))
+                , output_gates(std::exchange(m_circuit.output_gates, {}))
+                , next_gate_id(m_circuit.gates.size())
+        {
+        }
+
+        MutableCircuit(std::vector<MutableNode> const& m_nodes, 
+                    GateIdContainer const& inputs,
+                    GateIdContainer const& outputs)
+                    : gates(m_nodes)
+                    , input_gates(inputs)
+                    , output_gates(outputs)
+                    , next_gate_id(m_nodes.size())
+        {
+        }
+
+        MutableCircuit(std::vector<MutableNode> const& m_nodes, 
+                    GateIdContainer&& inputs,
+                    GateIdContainer&& outputs)
+                    : gates(m_nodes)
+                    , input_gates{std::move(inputs)}
+                    , output_gates{std::move(outputs)}
+                    , next_gate_id(m_nodes.size())
+        {
+        }
+        
+        void addGate(GateType const& g_type, 
+                    GateIdContainer const& operands = {}, 
+                    GateIdContainer const& users = {},
+                    bool is_output = false)
+        {
+            GateId new_id = next_gate_id++;
+            gates.emplace_back(MutableNode(new_id, g_type, operands, users));
+            if (g_type == GateType::INPUT)
+            {
+                input_gates.push_back(new_id);
+            }
+            if (is_output)
+            {
+                output_gates.push_back(new_id);
+            }
+        }
+
+        void addGate(MutableNode const& m_node, bool is_output = false)
+        {
+            GateId new_id = next_gate_id++;
+            gates.emplace_back(MutableNode(new_id, m_node.getType(), m_node.getOperands(), m_node.getUsers()));
+
+            if (m_node.getType() == GateType::INPUT)
+            {
+                input_gates.push_back(new_id);
+            }
+            if (is_output)
+            {
+                output_gates.push_back(new_id);
+            }
+        }
+
+        void connectGates(GateId new_operand_gate, GateId new_user_gate)
+        {
+            gates.at(new_user_gate).addOperand(new_operand_gate);
+            gates.at(new_operand_gate).addUser(new_user_gate);
+        }
+
+        void connectGates(GateIdContainer const& new_operands_gates, GateId new_user_gate)
+        {
+            gates.at(new_user_gate).addOperandsContainer(new_operands_gates);
+            for (GateId new_operand_gate : new_operands_gates)
+            {
+                gates.at(new_operand_gate).addUser(new_user_gate);
+            }
+        }
+
+        void connectGates(GateId new_operand_gate, GateIdContainer const& new_users_gates)
+        {
+            gates.at(new_operand_gate).addUsersContainer(new_users_gates);
+            for (GateId new_user_gate : new_users_gates)
+            {
+                gates.at(new_user_gate).addOperand(new_operand_gate);
+            }
+        }
+        
+        void changeGateType(GateId gate_id, GateType new_type)
+        {
+            gates.at(gate_id).setType(new_type);
+        }
+
+        void clearGateOperands(GateId gate_id)
+        {
+            gates.at(gate_id).setOperands({});
+        }
+
+        void clearGateUsers(GateId gate_id)
+        {
+            gates.at(gate_id).setUsers({});
+        }
+
+        void removeFromInputs(GateId removing_gate_id)
+        {
+            auto it = std::ranges::find(input_gates, removing_gate_id);
+            if (it != input_gates.end())
+            {
+                input_gates.erase(it);
+            }
+        }
+
+        void removeFromOutputs(GateId removing_gate_id)
+        {
+            auto it = std::ranges::find(output_gates, removing_gate_id);
+            if (it != output_gates.end())
+            {
+                output_gates.erase(it);
+            }
+        }
+
+        void updateIds()
+        {
+            std::unordered_map<GateId, GateId> id_updating;
+            for (size_t i = 0; i < gates.size(); ++i)
+            {
+                id_updating[gates[i].getId()] = i;
+                gates[i].setId(i);
+            }
+
+            for (MutableNode& gate : gates)
+            {
+                GateIdContainer updated_operands_id;
+                GateIdContainer updated_users_id;
+                for (GateId old_operand_id : gate.getOperands())
+                {
+                    updated_operands_id.push_back(id_updating[old_operand_id]);
+                }
+                for (GateId old_user_id : gate.getUsers())
+                {
+                    updated_users_id.push_back(id_updating[old_user_id]);
+                }
+                gate.setOperands(updated_operands_id);
+                gate.setUsers(updated_users_id);
+            }
+        }
+
+        void removeGate(GateId removing_gate_id)
+        {
+            auto it = std::ranges::find_if(gates, [removing_gate_id](MutableNode const& m_node) {
+                return m_node.getId() == removing_gate_id;
+            });
+            if (it != gates.end())
+            {
+                for (GateId gate_user_id : gates[removing_gate_id].getUsers())
+                { 
+                    gates[gate_user_id].removeIdFromOperands(removing_gate_id);
+                }
+                for (GateId gate_operand_id : gates[removing_gate_id].getOperands())
+                { 
+                    gates[gate_operand_id].removeIdFromUsers(removing_gate_id);
+                }
+                clearGateOperands(removing_gate_id);
+                clearGateUsers(removing_gate_id);
+                gates.erase(it);
+                removeFromInputs(removing_gate_id);
+                removeFromOutputs(removing_gate_id);
+
+                updateIds();
+            }
+        }
+    };
 }
